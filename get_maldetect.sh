912c1708837223cdf42b765167c36023d52de185
John E Malmberg <john.e.malmberg@intel.com>
2020-05-08 09:03:54 -0500

    CORCI-918 build: Add retry to download
    Signed-off-by: John E Malmberg <john.e.malmberg@intel.com>

--- get_maldetect.sh
+++ get_maldetect.sh
@@ -5,7 +5,8 @@ set -uex
 lmd_tarball='maldetect-current.tar.gz'
 
 curl "http://rfxn.com/downloads/${lmd_tarball}" \
-  -z "${lmd_tarball}"  --silent --show-error --fail -O
+  -z "${lmd_tarball}" --retry 10 --retry-max-time 60 --silent --show-error \
+  --fail -O
 
 # sanity check to make sure we have a good tarball
 tar -tf "${lmd_tarball}" > /dev/null
